a539604714d404fb8ae692b109c8713544aff112
nix <nik.nesterenko.010@gmail.com>
2023-03-21 02:58:14 +0200

    Добавил ДЗ6

--- main.js
+++ main.js
@@ -1,81 +1,106 @@
-let lineOfNumber = '';
-let correctNumber = 0;
-// 1
-for (let i = 10; i <= 20; i++) {
-    lineOfNumber += i + ', ';
-}
+const array = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
-console.log(lineOfNumber);
+let sum = 0,
+    positiveCount = 0,
+    negativeCount = 0,
+    positiveArray = [],
+    negativeArray = [];
 
-// 2
-lineOfNumber = '';
-for (let i = 10; i <= 20; i++) {
-    lineOfNumber += i * i + ', ';
-}
-
-console.log(lineOfNumber);
-
-// 3
-let item = 1;
-for (let i = 15; i <= 35; i++) {
-    item *= i;
-}
-console.log(item);
-
-// 4
-//var1
-correctNumber = 0;
-for (let i = 1; i <= 500; i++) {
-    correctNumber += i;
-}
-console.log(correctNumber/500);
-
-// var2
-correctNumber = 0;
-let arrayNumbers = [];
-for (let i = 1; i <= 500; i++) {
-    correctNumber += i;
-    arrayNumbers.push(i);
-}
-console.log(correctNumber/arrayNumbers.length);
-
-// 5
-correctNumber = 0;
-for (let i = 30; i <= 80; i++) {
-    if (Number.isInteger(i/2)) {
-        correctNumber += i;
-    }
-}
-console.log(correctNumber);
-
-// 6
-lineOfNumber = '';
-for (let i = 100; i <= 200; i++) {
-    if (Number.isInteger(i/3)) {
-        lineOfNumber += i + ', ';
-    }
-}
-console.log(lineOfNumber);
-
-// 7 - не уверен в том что я правильно понял задание
-
-let someNumber = Math.floor(Math.random() * (100 - 1)) + 1; // генерируем рандомное число от 2 до 100 для проверки
-console.log(someNumber);
-let isPrime = true;
-
-if (someNumber < 2) {
-  isPrime = false;
-}
-
-for (let i = 2; i < someNumber; i++) {
-  if (someNumber % i === 0) {
-    isPrime = false;
-    break;
+for(let i = 0; i < array.length; i++) {
+  if(array[i] >= 0) {
+    positiveArray.push(array[i]);
+    positiveCount++;
+  }else{
+    negativeArray.push(array[i]);
+    negativeCount++;
   }
 }
 
-if (isPrime) {
-  console.log(`${someNumber} простое число`);
-} else {
-  console.log(`${someNumber} не является простым числом`);
+console.log(positiveArray);
+console.log(negativeArray);
+
+for (let i = 0; i < positiveArray.length; i++) {
+  sum += positiveArray[i];  
 }
+
+console.log("Sum: " + sum);
+console.log("Positiv: " + positiveCount);
+
+
+// 2
+
+// first var
+let minimul = Math.min(...negativeArray);
+let minIndex = negativeArray.indexOf(minimul); 
+console.log(`minimum number is ${minimul} and his index is ${minIndex}`);
+
+// second var
+minimul = negativeArray[0];
+minIndex = 0;
+for(let i = 0; i < negativeArray.length; i++){
+  if (negativeArray[i] < minimul) {
+    minimul = negativeArray[i];
+    minIndex = i;
+  }
+}
+
+console.log(`minimum number is ${minimul} and his index is ${minIndex}`);
+
+// 3
+
+maximum = positiveArray[0];
+maxIndex = 0;
+for(let i = 0; i < positiveArray.length; i++){
+  if (positiveArray[i] > maximum) {
+    maximum = positiveArray[i];
+    maxIndex = i;
+  }
+}
+console.log(`maximum number is ${maximum} and his index is ${maxIndex}`);
+
+// 4 узнал рание 
+
+console.log(`Negative: ${negativeCount} or ${negativeArray.length}`);
+
+// 5, 6, 7, 8
+
+let ungepaart = 0,
+    paart = 0,
+    sumPaart = 0,
+    sumUngepaart = 0;
+for (let i = 0; i < positiveArray.length; i++) {
+  if (positiveArray[i] % 2) {
+    ungepaart++;
+    sumUngepaart += positiveArray[i];
+  }else{
+    paart++;
+    sumPaart += positiveArray[i];
+  }
+}
+console.log(`Непарных позитивных элементов массива: ${ungepaart}, парных позитивных ${paart}`);
+console.log(`Сумма парных позитивных элементов: ${sumPaart}, а сумма непарных позитивных элементов: ${sumUngepaart}`);
+
+// 9 использую forOf для разнобразия)))
+
+let multSum = 1;
+for (positiveItem of positiveArray) {
+  multSum *= positiveItem;
+}
+console.log(multSum);
+
+// 10
+
+let max = array[0];
+for (maxItem of array) {
+  if (maxItem > max) {
+    max = maxItem;
+  }
+}
+
+for(let i = 0; i < array.length; i++) {
+  if(array[i] !== max) {
+    array[i] = 0;
+  }
+}
+console.log(`Максимальное число в массиве это ${max}`);
+console.log(`Новый обнуленный массив: ${array}`);
